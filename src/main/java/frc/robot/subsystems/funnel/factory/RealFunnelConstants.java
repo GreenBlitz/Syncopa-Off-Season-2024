7b62e2e03be26bef84fd4749f4c3d726534d3c13
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-10-13 20:25:44 +0300

    added ir sensor

--- src/main/java/frc/robot/subsystems/funnel/factory/RealFunnelConstants.java
+++ src/main/java/frc/robot/subsystems/funnel/factory/RealFunnelConstants.java
@@ -5,6 +5,7 @@ import com.revrobotics.SparkLimitSwitch;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.IDs;
+import frc.robot.hardware.digitalinput.channeled.ChanneledDigitalInput;
 import frc.robot.hardware.digitalinput.supplied.SuppliedDigitalInput;
 import frc.robot.hardware.motor.sparkmax.BrushlessSparkMAXMotor;
 import frc.robot.hardware.motor.sparkmax.SparkMaxWrapper;
@@ -38,11 +39,13 @@ public class RealFunnelConstants {
 
 		SuppliedDoubleSignal voltageSignal = new SuppliedDoubleSignal("voltage", sparkMaxWrapper::getVoltage);
 
-		BooleanSupplier isPressed = () -> sparkMaxWrapper.getReverseLimitSwitch(REVERSE_LIMIT_SWITCH_TYPE).isPressed();
-		sparkMaxWrapper.getReverseLimitSwitch(REVERSE_LIMIT_SWITCH_TYPE).enableLimitSwitch(false);
-		SuppliedDigitalInput beamBreaker = new SuppliedDigitalInput(isPressed, DEBOUNCE_TYPE, DEBOUNCE_TIME_SECONDS);
+//		BooleanSupplier isPressed = () -> sparkMaxWrapper.getReverseLimitSwitch(REVERSE_LIMIT_SWITCH_TYPE).isPressed();
+//		sparkMaxWrapper.getReverseLimitSwitch(REVERSE_LIMIT_SWITCH_TYPE).enableLimitSwitch(false);
+//		SuppliedDigitalInput beamBreaker = new SuppliedDigitalInput(isPressed, DEBOUNCE_TYPE, DEBOUNCE_TIME_SECONDS);
 
-		return new FunnelStuff(logPath, motor, voltageSignal, beamBreaker);
+		ChanneledDigitalInput irSensor = new ChanneledDigitalInput(8,DEBOUNCE_TIME_SECONDS);
+
+		return new FunnelStuff(logPath, motor, voltageSignal, irSensor);
 	}
 
 }

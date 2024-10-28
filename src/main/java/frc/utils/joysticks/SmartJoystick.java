216e5a007607fae639799b0e81cc3f601f8bdecc
itamaroryan <itamar.oryan@gmail.com>
2024-10-28 13:10:23 +0200

    idk

--- src/main/java/frc/utils/joysticks/SmartJoystick.java
+++ src/main/java/frc/utils/joysticks/SmartJoystick.java
@@ -8,7 +8,8 @@ import edu.wpi.first.wpilibj2.command.button.POVButton;
 
 public class SmartJoystick {
 
-	private static final double DEADZONE = 0.11;
+	private static final double DEADZONE = 0.14
+			;
 	private static final double DEFAULT_THRESHOLD_FOR_AXIS_BUTTON = 0.1;
 	private static final double SENSITIVE_AXIS_VALUE_POWER = 2;
 
